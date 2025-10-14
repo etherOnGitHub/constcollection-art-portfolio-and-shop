81b8010764861865786af716325e0963503ca4f3
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-10-14 18:09:03 +0100

    fix views and url patterns

--- constcollection/settings.py
+++ constcollection/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.environ.get('HEROKU_LINK', 'default')]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://*.herokuapp.com', 
