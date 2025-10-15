5c0de009d5f75ac789da46ac977b9a21a55c56bd
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-10-15 14:59:23 +0100

    Add AboutPage URL route to urlpatterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, AboutPage
 
 urlpatterns = [
     path("", HomePage.as_view(template_name="index.html"), name="home"),
+    path("about/", AboutPage.as_view(), name="about"),
 ]
