7de01787159b0b3fe17a645069a1fa6326fc6f78
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-10-15 17:40:39 +0100

    remove redundant js code and fix bootstrap modal targets

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM fully loaded and parsed");
-
-    const showModal = (id) => {
-        const el = document.getElementById(id);
-        if (!el) return;
-        // close any open modals first
-        document.querySelectorAll(".modal.show").forEach(openModal => {
-            const instance = bootstrap.Modal.getInstance(openModal);
-            if (instance) instance.hide();
-        });
-        // then open the new one
-        const modal = new bootstrap.Modal(el);
-        modal.show();
-        console.log(`Modal with id ${id} shown`);
-    };   
 });
