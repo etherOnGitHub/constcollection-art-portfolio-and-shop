74065630d0649be107e218583534698542be4ecb
Rachel Davey <racheld9999@outlook.com>
2025-10-16 11:45:17 +0200

    feat: add gallery and artwork detail views with filtering and sorting options (unstyled)

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from . import views
 from .views import HomePage, AboutPage
 
 urlpatterns = [
     path("", HomePage.as_view(template_name="index.html"), name="home"),
     path("about/", AboutPage.as_view(), name="about"),
     path('summernote/', include('django_summernote.urls')),
+    path('gallery/', views.gallery, name='gallery'),
+    path('gallery/<slug:slug>/', views.artwork_detail, name='artwork_detail'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Artwork, Tag, Artist
 from django.views.generic import TemplateView
 
 
@@ -10,3 +11,36 @@ class HomePage(TemplateView):
 class AboutPage(TemplateView):
     """Displays the About page with artist bio, philosophy and process"""
     template_name = 'about.html'
+
+
+def gallery(request):
+    artworks = Artwork.objects.all()
+    tags = Tag.objects.all()
+    
+    # Gets tags 
+    tag_slug = request.GET.get('tag')
+    if tag_slug:
+        artworks = artworks.filter(tags__slug=tag_slug)
+
+    sort = request.GET.get('sort')
+    if sort == 'recent':
+        artworks = artworks.order_by('-created_at')
+    elif sort == 'price_low':
+        artworks = artworks.order_by('price')
+    elif sort == 'price_high':
+        artworks = artworks.order_by('-price')
+
+    context = {
+        'artworks': artworks,
+        'tags': tags,
+        'selected_tag': tag_slug,
+        'selected_sort': sort,
+    }
+    return render(request, 'gallery/gallery.html', context)
+
+def artwork_detail(request, slug):
+    artwork = get_object_or_404(Artwork, slug=slug)
+    context = {
+        'artwork': artwork,
+    }
+    return render(request, 'gallery/artwork_detail.html', context)
