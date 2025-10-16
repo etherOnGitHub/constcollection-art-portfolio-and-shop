c1bb8e22415c59a85059ea317518c6c1b8db879a
Rachel Davey <racheld9999@outlook.com>
2025-10-16 08:05:22 +0200

    register Artist and Artwork models in admin with custom display and search options

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Artist, Artwork
 # Register your models here.
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact')
+    search_fields = ('name',)
+
+
+@admin.register(Artwork)
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'price', 'is_available', 'created_at')
+    list_filter = ('is_available', 'artist', 'created_at')
+    search_fields = ('title', 'artist__name')
+    prepopulated_fields = {'slug': ('title',)}
