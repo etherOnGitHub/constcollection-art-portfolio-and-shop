c228cdf3cf0dd196c625fc14a4cf87076fe09994
Rachel Davey <racheld9999@outlook.com>
2025-10-16 13:45:14 +0200

    refactor: enhance ArtworkAdmin to include thumbnail display and improve field organization

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,23 +1,51 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from django.utils.html import format_html
+from cloudinary.utils import cloudinary_url
 from .models import Artist, Artwork, Tag
 # Register your models here.
 @admin.register(Artwork)
-class ArtworkAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'artist', 'price', 'is_available', 'created_at')
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = ('thumbnail_list', 'title', 'artist', 'price', 'is_available', 'created_at')
     list_filter = ('is_available', 'artist', 'created_at')
     search_fields = ('title', 'artist__name')
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('description',)
-
-    def thumbnail(self, obj):
-        if obj.image_url:
-            return format_html('<img src="{}" width="50" height="50" style="object-fit: cover; border-radius: 4px;" />', obj.image_url.url)
-        return "-"
     
-    thumbnail.short_description = 'Thumbnail'
-    thumbnail.admin_order_field = 'image_url'
+    readonly_fields = ('thumbnail_preview',)
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title',
+                'slug',
+                'artist',
+                'price',
+                'is_available',
+                'image_url',
+                'thumbnail_preview', 
+                'description',
+                'tags',
+            ),
+        }),
+    )
+
+    def thumbnail_list(self, obj):
+        if obj.image_url:
+            return format_html(
+                '<img src="{}" width="50" height="50" style="object-fit: cover; border-radius: 5px;" />',
+                obj.image_url.url
+            )
+        return "No Image"
+    thumbnail_list.short_description = "Thumbnail"
+
+    def thumbnail_preview(self, obj):
+        if obj.image_url:
+            return format_html(
+                '<img src="{}" width="300" style="object-fit: cover; border-radius: 10px;" />',
+                obj.image_url.url
+            )
+        return "No Image"
+    thumbnail_preview.short_description = "Current Thumbnail"
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ('name', 'contact')
