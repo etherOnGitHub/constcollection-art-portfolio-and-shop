97563ab8db87578577d1462efb47b8bdeaecfb5d
Rachel Davey <racheld9999@outlook.com>
2025-10-16 08:03:16 +0200

    add slug field and save method to Artist model

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
 # Create your models here.
 class Artist(models.Model):
@@ -11,6 +12,11 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 class Artwork(models.Model):
     art_id = models.AutoField(primary_key=True)
