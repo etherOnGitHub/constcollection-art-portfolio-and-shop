532402687feb75f111062ff39f7a5d26f97cbc73
Rachel Davey <racheld9999@outlook.com>
2025-10-18 06:10:37 +0200

    Refactor Exhibition model to use CloudinaryField for cover image and video

--- exhibitions/models.py
+++ exhibitions/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from cloudinary.models import CloudinaryField, CloudinaryResource, CloudinaryVideoField, CloudinaryImageField
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 class Exhibition(models.Model):
     title = models.CharField(max_length=200)
-    cover_image = CloudinaryImageField()
-    video = CloudinaryVideoField(blank=True, null=True)
+    cover_image = CloudinaryField('image')
+    video = CloudinaryField('video', resource_type='video', blank=True, null=True)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
