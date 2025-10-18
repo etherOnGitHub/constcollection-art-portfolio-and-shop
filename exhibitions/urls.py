c1aef116bbd8e3e5c4812b0d0757da4fece9ff2a
Rachel Davey <racheld9999@outlook.com>
2025-10-18 18:21:59 +0200

    Revert Exhibition views to function-based from class-based views

--- exhibitions/urls.py
+++ exhibitions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('exhibitions/', views.ExhibitionListView.as_view(), name='exhibition_list'),
-    path('exhibitions/<int:pk>/', views.ExhibitionDetailView.as_view(), name='exhibition_detail'),
+    path('exhibitions/', views.exhibition_list, name='exhibition_list'),
+    path('exhibitions/<int:pk>/', views.exhibition_detail, name='exhibition_detail'),
 ]
