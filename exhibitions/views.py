281e548fe9e9b1bb34e4234cea6a0c55565bbb09
Rachel Davey <racheld9999@outlook.com>
2025-10-18 06:02:45 +0200

    Add create, edit, and delete functionality for exhibitions with corresponding templates

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from exhibitions.forms import ExhibitionForm
 from .models import Exhibition
 from gallery.decorators import superuser_required_cbv, superuser_required
 
@@ -14,15 +16,27 @@ def exhibition_list(request):
 def exhibition_detail(request, pk):
     exhibition = Exhibition.objects.get(pk=pk)
     return render(request, 'exhibitions/exhibition_detail.html', {'exhibition': exhibition})
-    return render(request, 'exhibitions/exhibition_detail.html', {'pk': pk})
 
 @superuser_required
 def edit_exhibition(request, pk):
-    # Logic to edit an existing exhibition
-    return render(request, 'exhibitions/edit_exhibition.html', {'pk': pk})
+    exhibition = Exhibition.objects.get(pk=pk)
+    form = ExhibitionForm(request.POST or None, request.FILES or None, instance=exhibition)
+    if form.is_valid():
+        form.save()
+        return render(request, 'exhibitions/exhibition_detail.html', {'exhibition': exhibition})
+    return render(request, 'exhibitions/edit_exhibition.html', {'form': form, 'exhibition': exhibition})
 
 @superuser_required
 def create_exhibition(request):
-    # Logic to create a new exhibition
+    form = ExhibitionForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return render(request, 'exhibitions/exhibition_list.html')
+
     return render(request, 'exhibitions/create_exhibition.html')
 
+@superuser_required
+def delete_exhibition(request, pk):
+    exhibition = Exhibition.objects.get(pk=pk)
+    exhibition.delete()
+    return render(request, 'exhibitions/exhibition_list.html')
