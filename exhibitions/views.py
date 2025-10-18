0571aca493ff3135f4264b88ce8e0eb7115fd064
Rachel Davey <racheld9999@outlook.com>
2025-10-18 18:30:43 +0200

    Refactor exhibition views to use get_object_or_404 and streamline exhibition list rendering

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -1,42 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
 from exhibitions.forms import ExhibitionForm
 from .models import Exhibition
-from gallery.decorators import superuser_required_cbv, superuser_required
-
-# Create your views here.
+from gallery.decorators import superuser_required
 
 def exhibition_list(request):
-    exhibition = Exhibition.objects.all()
-    return render(request, 'exhibitions/exhibition_list.html', {'exhibition': exhibition})
+    exhibitions = Exhibition.objects.all()
+    return render(request, 'exhibitions/exhibition_list.html', {'exhibitions': exhibitions})
 
-    # Logic to retrieve and display a list of exhibitions
-    return render(request, 'exhibitions/exhibition_list.html')
-
+# View exhibition details
 def exhibition_detail(request, pk):
-    exhibition = Exhibition.objects.get(pk=pk)
+    exhibition = get_object_or_404(Exhibition, pk=pk)
     return render(request, 'exhibitions/exhibition_detail.html', {'exhibition': exhibition})
-
-@superuser_required
-def edit_exhibition(request, pk):
-    exhibition = Exhibition.objects.get(pk=pk)
-    form = ExhibitionForm(request.POST or None, request.FILES or None, instance=exhibition)
-    if form.is_valid():
-        form.save()
-        return render(request, 'exhibitions/exhibition_detail.html', {'exhibition': exhibition})
-    return render(request, 'exhibitions/edit_exhibition.html', {'form': form, 'exhibition': exhibition})
 
 @superuser_required
 def create_exhibition(request):
     form = ExhibitionForm(request.POST or None, request.FILES or None)
     if form.is_valid():
-        form.save()
-        return render(request, 'exhibitions/exhibition_list.html')
+        exhibition = form.save()
+        return redirect('exhibitions:exhibition_detail', pk=exhibition.pk)
+    return render(request, 'exhibitions/create_exhibition.html', {'form': form})
 
-    return render(request, 'exhibitions/create_exhibition.html')
+@superuser_required
+def edit_exhibition(request, pk):
+    exhibition = get_object_or_404(Exhibition, pk=pk)
+    form = ExhibitionForm(request.POST or None, request.FILES or None, instance=exhibition)
+    if form.is_valid():
+        form.save()
+        return redirect('exhibitions:exhibition_detail', pk=exhibition.pk)
+    return render(request, 'exhibitions/edit_exhibition.html', {'form': form, 'exhibition': exhibition})
 
 @superuser_required
 def delete_exhibition(request, pk):
-    exhibition = Exhibition.objects.get(pk=pk)
+    exhibition = get_object_or_404(Exhibition, pk=pk)
     exhibition.delete()
-    return render(request, 'exhibitions/exhibition_list.html')
+    return redirect('exhibitions:exhibition_list')
