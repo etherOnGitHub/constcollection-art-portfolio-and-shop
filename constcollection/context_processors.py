aef90524cae18382ccf75a208b46c72236d0f566
Rachel Davey <racheld9999@outlook.com>
2025-10-18 18:52:13 +0200

    Add upcoming exhibitions to footer for enhanced visibility

--- constcollection/context_processors.py
+++ constcollection/context_processors.py
@@ -1,4 +1,5 @@
 from allauth.account.forms import LoginForm, SignupForm
+from django.utils import timezone
 
 def auth_forms(request):
     """
@@ -7,4 +8,15 @@ def auth_forms(request):
     return {
         'login_form': LoginForm(),
         'signup_form': SignupForm(),
+    }
+
+def upcoming_exhibitions(request):
+    """
+    Makes upcoming exhibitions available to every template.
+    """
+    from exhibitions.models import Exhibition
+    today = timezone.now().date()
+    upcoming = Exhibition.objects.filter(start_date__gt=today).order_by('start_date')[:3]
+    return {
+        'upcoming_exhibitions': upcoming,
     }
