e974273fc61d5f0088470e827e55deaba0b201dd
Rachel Davey <racheld9999@outlook.com>
2025-10-18 05:48:43 +0200

    Create exhibition model

--- exhibitions/models.py
+++ exhibitions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField, CloudinaryResource, CloudinaryVideoField, CloudinaryImageField
 # Create your models here.
+
+class Exhibition(models.Model):
+    title = models.CharField(max_length=200)
+    cover_image = CloudinaryImageField()
+    video = CloudinaryVideoField(blank=True, null=True)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.title
